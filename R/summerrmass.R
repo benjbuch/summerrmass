95cfd8670f0377838aa6a42f5d20497e61dd7453
benjbuch <benjamin.buchmuller@gmail.com>
2021-05-15 12:31:06 +0200

    Add package description/glossary

---
+++ R/summerrmass.R
@@ -0,0 +1,47 @@
+#' summerrmass
+#'
+#' @author Benjamin Buchmuller \email{benjamin.buchmuller@@tu-dortmund.de}
+#'
+#' @title summerrmass
+#'
+#' @description
+#'
+#' Import and analyse peaks from MALDI spectra.
+#'
+#' @section General Functions:
+#'
+#' (These may be moved to a general package.)
+#'
+#' \subsection{Interaction}{
+#'
+#' \link{select_directory}
+#'
+#' \link{select_single_file}
+#'
+#' } % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+#'
+#' \subsection{Import Experiment Layouts}{
+#'
+#' \link{import_layout_from_excel}
+#'
+#' \link{import_layout_from_paths}
+#'
+#' } % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+#'
+#' \subsection{Formatting Helpers}{
+#'
+#' \link{as_well}
+#'
+#' } % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+#'
+#' @section Routines for MALDI peak calling from Burker FlexAnalysis:
+#'
+#' tbd
+#'
+#' @section Interactive Workflows:
+#'
+#' tbd
+#'
+#' @docType package
+#' @name summerrmass
+NULL
