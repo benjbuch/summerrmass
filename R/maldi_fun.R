e8b4597c57fa419ba18b0ecd4550a7650912888f
benjbuch <benjamin.buchmuller@gmail.com>
2021-05-15 22:08:50 +0200

    Add versatile maldi_tapply to user end

--- R/maldi_fun.R
+++ R/maldi_fun.R
@@ -14,6 +14,8 @@
 #' \code{pivot} must contain a regular expression to identify the well, i.e.,
 #' contain [A-Z]+[0-9]+".
 #'
+#' The resulting \link[dplyr:grouped_df]{grouped data frame} is grouped by \code{well}.
+#'
 #' @seealso \code{\link{import_layout_from_paths}}
 #'
 #' @importFrom magrittr %>%
@@ -31,7 +33,8 @@ import_layout_from_paths.maldi <- function(paths, pivot = "[0-9]_[A-Z]+[0-9]+",
 
   datam <- dplyr::mutate(datad, as_well(stringr::str_extract(pivot, well_regex),
                                         as.tibble = TRUE)) %>%
-    dplyr::arrange(.data$well, .data$sub_1, .by_group = TRUE)
+    dplyr::arrange(.data$well, .data$sub_1, .by_group = TRUE) %>%
+    dplyr::group_by(.data$well)
 
   attr(datam, "dir") <- attr(datad, "dir")
 
@@ -146,60 +149,145 @@ maldi_import_spectra <- function(path = getwd(), ...) {
 
 }
 
+#' Get the file paths of a MALDI spectrum as vector.
+#'
+#' @param object A (list of) \code{\link[MALDIquant:MassSpectrum-class]{MassSpectrum}}.
+#'
+maldi_get_paths <- function(object) {
+
+  sapply(object, function(x) MALDIquant::metaData(x)$file, USE.NAMES = FALSE)
+
+}
+
 #  PROCESSING MASS SPECTRA -----------------------------------------------------
 
-#' Align and average MALDI spectra over repeated same-well measurements
+#' Align and average MALDI spectra
 #'
-#' @param object A list of \code{\link[MALDIquant:MassSpectrum-class]{MassSpectrum}}.
+#' Default method to align and average MALDI spectra.
+#'
+#' @inheritParams maldi_get_paths
 #' @param method_baseline Method for background subtraction.
 #' See \link[MALDIquant:removeBaseline]{MALDIquant::removeBaseline}.
 #' @param method_average Method to average replicate measurements.
 #' See \link[MALDIquant:averageMassSpectra]{MALDIquant::averageMassSpectra}.
 #' @param final_trim_range A numeric vector from which the maximum and the minimum
 #' value are taken to trim the spectrum to a relevant range.
-#' @param ... further arguments passed to \code{\link[MALDIquant:alignSpectra]{alignSpectra}}
+#' @param ... Arguments passed to \code{\link[MALDIquant:alignSpectra]{alignSpectra}}.
+#'
+#' @return A single \code{\link[MALDIquant:MassSpectrum-class]{MassSpectrum}}.
+#'
+#' @details
+#' The baseline of the spectra is removed using \code{method_baseline} first,
+#' then the spectra aligned using the default parameters of
+#' \code{\link[MALDIquant:alignSpectra]{alignSpectra}} unless other arguments
+#' are passed via \code{...}. The aligned spectra are averaged and trimmed to
+#' \code{final_trim_range}.
+#'
+#' This method can be substituted/overwritten to adapt specific needs in a pipeline.
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+maldi_average_spectra <- function(object,
+                                  method_baseline = "SNIP",
+                                  method_average  = "mean",
+                                  ...,
+                                  final_trim_range = c(0, Inf)) {
+
+  object  %>%
+    # remove baseline
+    MALDIquant::removeBaseline(
+      method = method_baseline) %>%
+    # align spectra in case of repeated measurements ...
+    MALDIquant::alignSpectra(...) %>%
+    # ... and average them
+    MALDIquant::averageMassSpectra(
+      method = method_average) %>%
+    # trim spectra
+    MALDIquant::trim(range = final_trim_range)
+
+}
+
+#' Apply a function to a group of MALDI spectra
+#'
+#' @inheritParams maldi_get_paths
+#' @inheritParams base::tapply
+#' @param MoreArgs A list of parameters passed to \code{FUN}.
+#'
+#' @details
+#' \code{FUN} is called with \code{MoreArgs} along the mass spectrum list \code{object}.
+#' Thereby, \code{FUN} is called upon groups of \code{object}, if a grouping can
+#' be established; else, \code{FUN} is applied for each element of \code{object}
+#' separately.
+#'
+#' To establish the grouping, \code{INDEX} is used.
+#'
+#' \itemize{
+#' \item{If \code{INDEX} is a \code{\link[base:data.frame]{data.frame}} or a similar
+#' object such as a \code{\link[tibble:tibble]{tibble}}, the number and order of
+#' the rows must correspond to the number and order of the elements in \code{object}.
+#' }
+#' \item{If \code{INDEX} qualifies as list of \code{\link[base:factor]{factor}}s,
+#' each of the same length as \code{object}, this is used as grouping.
+#' }
+#' \item{If \code{INDEX} is \code{NULL}, an attempt is made to extract the
+#' experiment layout from the file paths stored in the spectra (see
+#' \link{import_layout_from_paths.maldi}) and the function is applied by well,
+#' i.e., by a portion of the non-unique file path that matches \code{"[A-Z]+[0-9]+"}}.
+#' }
+#'
+#' @return Depends on \code{FUN}.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @importFrom rlang !!!
+#'
+#' @export
+maldi_tapply <- function(object, FUN,
+                         INDEX = NULL, MoreArgs = list()) {
+
+  if (is.null(INDEX)) {
+
+    INDEX <- import_layout_from_paths.maldi(maldi_get_paths(object),
+                                            relative_to = attr(object, "dir")) %>%
+      # it is important to pass the index along the order of the MassSpectrum list
+      dplyr::arrange(.data$findex) %>%
+      dplyr::group_indices()
+
+  }
+
+  log_debugging("INDEX is", object = INDEX)
+
+  eval(rlang::call2(tapply, X = object, INDEX = INDEX, FUN = FUN, !!!MoreArgs))
+
+}
+
+#' Align and average MALDI spectra by well
+#'
+#' Applies \link{maldi_average_spectra} to each group of wells using \link{maldi_tapply}.
+#'
+#' @inheritParams maldi_tapply
+#' @inheritParams maldi_average_spectra
 #'
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
 #'
 #' @export
-maldi_align_by_well <- function(object,
-                                method_baseline = "SNIP",
-                                method_average  = "mean",
-                                ...,
-                                final_trim_range = c(0, Inf)) {
+maldi_average_by_well <- function(object,
+                                  INDEX = NULL,
+                                  method_baseline = "SNIP",
+                                  method_average  = "mean",
+                                  ...,
+                                  final_trim_range = c(0, Inf)) {
 
-  log_process("pre-processing spectra by well")
+  log_process("averaging and aligning spectra")
 
-
-  relative_to <- attr(object, "dir")
-
-  if (is.null(relative_to)) relative_to <- getwd()
-
-  object %>% {tapply(., sapply(., function(x) MALDIquant::metaData(x)$file,
-                               USE.NAMES = FALSE) %>%
-                       import_layout_from_paths.maldi(relative_to = relative_to) %>%
-                       # this is important to pass the grouping index along the
-                       # order of the MassSpectrum list
-                       dplyr::arrange(.data$well) %>%
-                       # average by repeated measurements
-                       dplyr::ungroup(.data$replicate) %>%
-                       # use groups to process by wells
-                       dplyr::group_indices(), function(y) {
-
-                         y  %>%
-                           # remove baseline
-                           MALDIquant::removeBaseline(
-                             method = "SNIP") %>%
-                           # align spectra in case of repeated measurements ...
-                           MALDIquant::alignSpectra(...) %>%
-                           # ... and average them
-                           MALDIquant::averageMassSpectra(
-                             method = method_average) %>%
-                           # trim spectra
-                           MALDIquant::trim(range = final_trim_range)
-
-                       })} -> data_mzxml
+  data_mzxml <- maldi_tapply(object = object, FUN = maldi_average_spectra,
+                             INDEX = INDEX,
+                             MoreArgs = list(method_baseline = method_baseline,
+                                             method_average  = method_average,
+                                             ...,
+                                             final_trim_range = final_trim_range))
 
   log_done()
 
@@ -208,3 +296,4 @@ maldi_align_by_well <- function(object,
   data_mzxml
 
 }
+
