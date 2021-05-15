6ae7b289f2fc1308b97f7c53ab47139a21f1379a
benjbuch <benjamin.buchmuller@gmail.com>
2021-05-15 16:17:02 +0200

    Add loggers for routine messaging

--- R/fun_loggers.R
+++ R/fun_loggers.R
@@ -1,17 +1,50 @@
-log_message <- function(...) {
+log_task <- function(...) {
 
   if (getOption("verbose", default = TRUE)) {
 
     rlang::inform(message = rlang::format_error_bullets(
-      c(i = paste(...))
+      c(i = paste0(stringr::str_to_sentence(paste(...)), "."))
     ))
 
   }
 
 }
 
-log_error <- function(...) {
+log_process <- function(...) {
 
+  if (getOption("verbose", default = TRUE)) {
+
+    rlang::inform(message = rlang::format_error_bullets(
+      c(i = paste0("... ", paste(...), "."))
+    ))
+
+  }
+
+}
+
+log_done <- function() {
+
+  log_process("done")
+
+}
+
+log_message <- function(...) {
+
+  if (getOption("verbose", default = TRUE)) {
+
+    rlang::inform(message = rlang::format_error_bullets(
+      c(i = paste("...", ...))
+    ))
+
+  }
+
+}
+
+log_error <- function(header, body) {
+
+  rlang::abort(message = paste0(
+    stringr::str_to_sentence(header), "\n",
+    rlang::format_error_bullets(body)))
 
 }
 
@@ -23,7 +56,7 @@ log_debugging <- function(...) {
 
     rlang::inform(message = rlang::format_error_bullets(
       paste0(
-        "", fn_call, ": ", paste(...))
+        "", fn_call, ": ", stringr::str_to_sentence(paste(...)), ".")
       ))
 
   }
