e16ac94d2cdef87a99dd6a8a7dee08aa73e7af2a
benjbuch <benjamin.buchmuller@gmail.com>
2021-05-18 20:02:33 +0200

    Update batch pipeline

--- R/maldi_interface.R
+++ R/maldi_interface.R
@@ -12,13 +12,13 @@
 #' @inheritParams import_layout_from_paths.maldi
 #' @param manual ...
 #' @param review ...
-#' @param layout_file,confirm_layout_file,layout_import_FUN Path to a file that
+#' @param layout_file,confirm_layout_file,FUN_import_layout Path to a file that
 #' contains metadata associated with each well/group. \code{layout_FUN} is applied
 #' to \code{layout_file} before it is joined with the measurement result. Must
 #' share column names with \link{import_layout_from_paths.maldi}. See Details.
 #' @param FUN_spect,MoreArgs_spect The function to be applied for pre-processing
 #' the mass spectra using \code{MoreArgs_spect}.
-#' @param FUN_peak,MoreArgs_peak The function to be applied for peak detection
+#' @param FUN_peaks,MoreArgs_peaks The function to be applied for peak detection
 #' using \code{MoreArgs_peak}.
 #' @inheritParams backup_file
 #'
@@ -41,9 +41,12 @@
 #' or \code{stored_spect.generic}, these analyses can be imported for the next stage;
 #' if not, the original files are renamed with a timestamp and new files added.
 #'
-#' \subsection{Adapting the pipeline}
+#' \subsection{Adapting the pipeline}{
 #'
 #' ...
+#'
+#' \code{FUN_spect}, \code{FUN_peaks} and \code{layout_import_FUN} is can be of
+#' type character or closure.
 #'
 #' In case \code{FUN_spect} or \code{FUN_peaks} use a function that must determine,
 #' e.g. which spectra are repeated measurements from a single well, which is
@@ -51,7 +54,9 @@
 #' the \code{pivot} regex may need to be updated in the corresponding \code{MoreArgs_...}
 #' arguments as well. A warning is emitted if it is not.
 #'
-#' \subsection{Joining with metadata from external plate layout}
+#' }
+#'
+#' \subsection{Joining with metadata from external plate layout}{
 #'
 #' If \code{layout_file} is \code{NULL}, the results will not be joined with an
 #' external file that holds associated metadata. (Skip this section.)
@@ -82,6 +87,8 @@
 #' If you provide such a (long) table directly, provide \code{\link{readxl::read_excel}}
 #' or another appropriate function as argument \code{layout_import_FUN}.
 #'
+#' }
+#'
 #' @return A list with one element for each group.
 #'
 #' @importFrom magrittr %>%
@@ -94,11 +101,14 @@ maldi_batch <- function(path = NULL,
                         confirm_layout_file = TRUE,
                         manual = FALSE,
                         review = TRUE,
-                        FUN_spect = "maldi_average_by_well",
+                        FUN_spect = maldi_average_by_well,
                         MoreArgs_spect = list(pivot = "[0-9]_[A-Z]+[0-9]+"),
-                        FUN_peaks = "maldi_peaks_by_well",
+                        FUN_peaks = maldi_find_peaks_by_well,
                         MoreArgs_peaks = list(pivot = "[0-9]_[A-Z]+[0-9]+"),
-                        layout_import_FUN = "import_layout_from_excel",
+                        FUN_draw  = maldi_draw_peaks_by_well,
+                        MoreArgs_draw  = list(),
+                        FUN_import_layout = import_layout_from_excel,
+                        MoreArgs_layout = list(),
                         stored_peaks.generic = "maldi_peaks.rds",
                         stored_spect.generic = "maldi_specs.rds",
                         stamp = "%y%m%d"
@@ -173,6 +183,36 @@ maldi_batch <- function(path = NULL,
 
   log_object(data_in_path)
 
+  # look for plate layout file in main path
+
+  curr_layout <- NULL
+
+  if (!is.null(layout_file)) {
+
+    # file.exists will expand to the current working directory if no full path
+    # is given.
+
+    if (file.exists(layout_file)) {
+
+      if (confirm_layout_file) {
+
+        use_layout_file <- askYesNo(paste("Do you want to apply", sQuote(layout_file),
+                                          "as layout to all groups in the directory?"))
+
+        if (use_layout_file) curr_layout <- normalizePath(layout_file)
+
+      } else {
+
+        # just take it
+
+        curr_layout <- normalizePath(layout_file)
+
+      }
+
+    }
+
+  }
+
   for (curr_group_idx in seq_along(data_in_path$path_to_group)) {
 
     log_line("-")
@@ -238,7 +278,7 @@ maldi_batch <- function(path = NULL,
 
     if (use_s) {
 
-      dat_s <- readRDS(file = pth_s); attr(dat_s, "dir") <- pth_s
+      dat_s <- readRDS(file = pth_s); attr(dat_s, "dir") <- curr_group_path
 
       log_process("imported", length(rds_s), "spectra from backup")
 
@@ -247,11 +287,13 @@ maldi_batch <- function(path = NULL,
       # keep a backup of existing spectra or peak files
       if (has_s) backup_file(file.path(curr_group_path, stored_spect.generic))
 
+      log_process("importing mass spectra")
+
       err_s <- tryCatch({
 
         dat_s <- maldi_import_spectra(path = curr_group_path)
 
-        eval(rlang::call2(FUN_spect, object = dat_s, !!!MoreArgs_spect))
+        dat_s <- eval(rlang::call2(FUN_spect, object = dat_s, !!!MoreArgs_spect))
 
         saveRDS(object = dat_s, file = pth_s)
 
@@ -274,6 +316,13 @@ maldi_batch <- function(path = NULL,
 
       }
 
+      log_process("saving")
+
+      attr(dat_s, "dir") <- curr_group_path
+      saveRDS(dat_s, file = pth_s)
+
+      log_done()
+
     }
 
     # processing peaks
@@ -282,7 +331,7 @@ maldi_batch <- function(path = NULL,
 
     if (use_p) {
 
-      dat_p <- readRDS(file = pth_p)
+      dat_p <- readRDS(file = pth_p); attr(dat_p, "dir") <- curr_group_path
 
       log_process("imported", scales::comma(sum(lengths(dat_p))),
                   "peak assignments from backup")
@@ -292,9 +341,131 @@ maldi_batch <- function(path = NULL,
       # keep a backup of existing spectra or peak files
       if (has_p) backup_file(file.path(curr_group_path, stored_peaks.generic))
 
+      # PEAK DETECTION 1: detect peaks automatically
+
+      log_process("detecting peaks by well")
+
+      dat_p <- eval(rlang::call2(FUN_peaks, object = dat_s, !!!MoreArgs_peaks))
+
+      # draw overview spectra
+
+      tmp_pdf <- paste0(stored_peaks.generic, "_tmp.pdf")
+
+      eval(rlang::call2(FUN_draw, object = dat_s, data_peaks = dat_p,
+                        file = normalizePath(file.path(curr_group_path, tmp_pdf)),
+                        !!!MoreArgs_draw))
+
+      # PEAK DETECTION 2: detect peaks manually
+
+      if (rstudioapi::isAvailable() && review) {
+
+        rstudioapi::viewer(tmp_pdf)
+
+        curr_check <- readline(paste("For which wells of", sQuote(curr_group),
+                                     "do you want manually assign peaks? "))
+
+        curr_check <- curr_check %>%
+          strsplit(split = "[^[:alnum:]]+") %>% unlist()
+
+        curr_check <- dat_p %>%
+          dplyr::filter(.data$well %in% curr_check) %>%
+          dplyr::pull(.data$findex) %>%
+          unique()
+
+        if (length(curr_check) > 0) {
+
+          dat_p.use <- dat_p %>%
+            dplyr::filter(!(.data$findex %in% curr_check))
+
+          dat_p.new <- eval(rlang::call2(
+            FUN_peaks, object = dat_s[curr_check],
+            !!!c(MoreArgs_peaks[which(names(MoreArgs_peaks) != "manual")],
+                 list(manual = TRUE))))
+
+          dat_p <- dplyr::bind_rows(dat_p.use, dat_p.new) %>%
+            dplyr::arrange(.data$findex)
+
+        }
+
+      }
+
+      file.remove(tmp_pdf)
+
+      log_process("saving")
+
+      attr(dat_p, "dir") <- curr_group_path
+      saveRDS(dat_p, file = pth_p)
+
+      log_done()
 
     }
 
+    ### dat_s and dat_p are now complete
+
+    log_task("combining with plate metadata")
+
+    # look for plate layout file in main path
+
+    if (!is.null(curr_layout)) {
+
+      log_process("looking for plate metadata")
+
+      if (!is.null(layout_file)) {
+
+        # file.exists will expand to the current working directory if no full path
+        # is given.
+
+        if (file.exists(layout_file)) {
+
+          if (confirm_layout_file) {
+
+            use_layout_file <- askYesNo(paste("Do you want to apply", sQuote(layout_file),
+                                              "as layout to all groups in the directory?"))
+
+            if (use_layout_file) curr_layout <- normalizePath(layout_file)
+
+          } else {
+
+            # just take it
+
+            curr_layout <- normalizePath(layout_file)
+
+          }
+
+        }
+
+      }
+
+      # if none of the above worked out, prompt to select a file; cancelling the
+      # dialogue will keep length(curr_layout) == 0.
+
+      if (is.null(curr_layout)) curr_layout <- select_single_file(path = curr_group)
+
+    }
+
+    if (length(curr_layout) == 0) {
+
+      log_process("no plate layout selected")
+
+      # save spectra by
+
+    } else {
+
+      dat_l <- rlang::call2(FUN_import_layout, file = normalizePath(curr_layout),
+                            !!!MoreArgs_layout)
+
+      dat_p <- dplyr::full_join(dat_p, dat_l)
+
+      # save spectra by content
+
+
+
+    }
+
+    data_in_path[i, "data_spectra"] <- dat_s
+    data_in_path[i, "data_peaks"] <- dat_p
+
+    log_line("-")
 
   } ### end of for each group loop
 
