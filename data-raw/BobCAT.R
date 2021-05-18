e7487affb4f93b43c652da023e94e4e6e007f9ee
benjbuch <benjamin.buchmuller@gmail.com>
2021-05-18 19:09:03 +0200

    Remove personal information from file paths in toy data

--- data-raw/BobCAT.R
+++ data-raw/BobCAT.R
@@ -2,4 +2,21 @@
 
 BobCAT <- maldi_import_spectra(path = "data-raw/BobCAT/")
 
+# replace path information to refelect current working directory as "."
+
+purge_personal_information <- function(x, old_path, new_path = ".") {
+
+  y <- x
+
+  MALDIquant::metaData(y)$parentFile <- ""
+  MALDIquant::metaData(y)$file <- sub(MALDIquant::metaData(x)$file,
+                                      pattern = old_path, replacement = new_path,
+                                      fixed = TRUE)
+
+  y
+
+}
+
+BobCAT <- lapply(BobCAT, purge_personal_information, old_path = attr(BobCAT, "dir"))
+
 usethis::use_data(BobCAT, overwrite = TRUE)
